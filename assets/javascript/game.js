028912258127dde87bdc36b8e45569c5e410d3db
ematt44 <ericmatthews133@gmail.com>
2017-11-27 17:18:06 -0800

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,12 +25,12 @@ var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
   // Lists the letters the user hased guessed
 
   guessedLetters.push(userGuess);
-  console.log(userGuess)
+  
 
   // // Randomly chooses a choice from the computerChoices array. (computer's guess)
 
   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  console.log(computerGuess);
+  
 
   // Call a function to start the game over
 
@@ -45,19 +45,19 @@ var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 
 
-  // If the user guesses right lette tell them thy are a psychic and start the game over
+  // If the user guesses right tell them they are a psychic and start the game over, same when the user loses
 
 
   if (userGuess == computerGuess) {
     wins++;
     console.log(wins);
-    alert("You're Psychic!!");
+    alert("You're Psychic!");
     // The game will start over
     reset(guesses, guessesLeft, guessedLetters);
 
   } else {
     guessesLeft--;
-    console.log(guessesLeft);
+    
   }
 
   if (guessesLeft == 0) {
@@ -71,16 +71,16 @@ var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 
   var html =
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guess Left: " + guessesLeft + "</p>" +
-    "<p>Guessed Letters: " + userGuess + "</p>";
-
-  // Set the inner HTML contents of the #game div to our html string
+    "<br><p>Wins:  " + wins + "</p>" +
+    "<p>Losses:  "  + losses + "</p>" +
+    "<p>Guesses Left:  " + guessesLeft + "</p>";
+   
+  // Set the inner HTML contents of the game
+ 
   document.querySelector("#game").innerHTML = html;
 
 
-  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(", ");
+  document.querySelector('#let').innerHTML = "Your Guesses so far:  " + guessedLetters.join(", ");
 
 
 
